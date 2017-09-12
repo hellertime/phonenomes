88ddf1c3f287d32ca26ad12aa3bec1a62a631db3
Chris Heller <hellertime@gmail.com>
2017-09-11 21:14:36 -0400

    Use aho-corasick to match patterns

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::io::prelude::*;
 use std::slice::Iter;
 
 #[macro_use] extern crate itertools;
-extern crate radix_trie;
 
-use radix_trie::Trie;
+extern crate aho_corasick;
+use aho_corasick::{Automaton, AcAutomaton, MatchesOverlapping, Match};
 
 // The mapping from digit to potential chars
-const phoneChars: [&[u8]; 10] = [
+const phone_chars: [&[u8]; 10] = [
     b"----", // 0 - no mappable chars
     b"----", // 1 - no mappable chars
     b"abc", // 2 
@@ -21,23 +21,23 @@ const phoneChars: [&[u8]; 10] = [
     b"wxyz" // 9
  ];
 
-// Computes the arrays from `phoneChars` which correspond to the 7 inputs
-fn phoneToChars<'a>(phone: Vec<u8>) -> Vec<&'a [u8]> {
+// Computes the arrays from `phone_chars` which correspond to the 7 inputs
+fn phone_to_chars<'a>(phone: Vec<u8>) -> Vec<&'a [u8]> {
     let digits : Vec<u32> = phone.iter().map(|c| (*c as char).to_digit(10).unwrap_or(0)).collect();
     vec![
-        phoneChars.get(digits[0] as usize).unwrap(),
-        phoneChars.get(digits[1] as usize).unwrap(),
-        phoneChars.get(digits[2] as usize).unwrap(),
-        phoneChars.get(digits[3] as usize).unwrap(),
-        phoneChars.get(digits[4] as usize).unwrap(),
-        phoneChars.get(digits[5] as usize).unwrap(),
-        phoneChars.get(digits[6] as usize).unwrap()]
+        phone_chars.get(digits[0] as usize).unwrap(),
+        phone_chars.get(digits[1] as usize).unwrap(),
+        phone_chars.get(digits[2] as usize).unwrap(),
+        phone_chars.get(digits[3] as usize).unwrap(),
+        phone_chars.get(digits[4] as usize).unwrap(),
+        phone_chars.get(digits[5] as usize).unwrap(),
+        phone_chars.get(digits[6] as usize).unwrap()]
 }
 
 // Computes the cartesian product of the input vector
-fn phoneToWords(input: Vec<u8>) -> Vec<String> {
+fn phone_to_words(input: Vec<u8>) -> Vec<String> {
     let mut output = Vec::new();
-    let mut cs : Vec<Iter<u8>> = phoneToChars(input).iter().map(|a| a.into_iter()).collect();
+    let mut cs : Vec<Iter<u8>> = phone_to_chars(input).iter().map(|a| a.into_iter()).collect();
     // iproduct wants an Iterator
     // but if we use an index on the Vec we induce a move, and we fail to compile
     // if we take a reference to the index we now have a &std::slice::Iter and not an Iterator
@@ -57,24 +57,53 @@ fn phoneToWords(input: Vec<u8>) -> Vec<String> {
     output
 }
 
-fn main() {
-    let words: Vec<&str> = include_str!("../res/wordlist.txt").lines().collect();
-    let mut trie: Trie<&str, &str> = Trie::new();
+fn is_covered<'a>(ms: MatchesOverlapping<&'a str, AcAutomaton<&'a str>>, spanning: usize) -> bool {
+    let mut low = 0;
+    let mut hi = 0;
+    let mut span = 0;
 
-    for w in words {
-        trie.insert(w, w);
+    for m in ms {
+        //println!(">low: {}; hi: {}; span: {}; match {:?}", low, hi, span, m);
+        if m.start > hi {
+            low = m.start;
+            hi = m.end;
+        }
+        else if m.start == hi {
+            span = span + (hi - low);
+            low = m.start;
+            hi = m.end;
+        } else if m.start <= low {
+            low = m.start;
+            if m.end > hi {
+                hi = m.end;
+            }
+        }
+        //println!("<low: {}; hi: {}; span: {}; match {:?}", low, hi, span, m);
     }
 
+    span = span + (hi - low);
+    //println!("=low: {}; hi: {}; span: {}", low, hi, span);
+
+    span == spanning
+}
+
+fn main() {
+    println!("Loading wordlist...");
+    let words: Vec<&str> = include_str!("../res/wordlist2.txt").lines().collect();
+
+    println!("Compiline automaton...");
+    let aut = aho_corasick::AcAutomaton::new(words);
+
     let stdin = io::stdin();
+    println!("Ready...");
     for line in stdin.lock().lines() {
         let line = line.unwrap();
         assert_eq!(7, line.len());
         println!("Words from {}", line);
-        let ws = phoneToWords(line.into_bytes());
-        for w in ws {
-            match trie.get(w.as_str()) {
-                None => println!("NONE"),
-                Some(_) => println!("{}",w)
+        let ws = phone_to_words(line.into_bytes());
+        for w in ws { //.iter().filter(|w| w.as_str() == "redtuba") {
+            if is_covered(aut.find_overlapping(&w), 7) {
+                println!("{}", w);
             }
         }
     }
